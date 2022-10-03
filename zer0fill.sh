7a5daa88ba0cfe7fe309dfe3553c2ccf2485c266
MistaBlitz <cooperm007@icloud.com>
2022-10-03 11:58:26 -0500

    added comments to code, revised echos

--- zer0fill.sh
+++ zer0fill.sh
@@ -4,18 +4,25 @@ echo "please enter drive letter (sdX)"
 read drivenum
 echo "are you sure you want to wipe $drivenum? y/n"
 read check
+# checking if user input is correct
 if [ "${check}" == "n" ];
 then
-	echo "answer no"
+	echo "user responded no"
 	 exit
+# checking if user input is correct
 elif [ "${check}" == "y" ];
 then
+	# setting date variable to todays date
 	date=$(date)
+	# setting whoami variable to user running script
 	whoami=$(whoami)
+	# writing information to log file
 	echo "Name: $whoami Date: $date Drive: $drivenum" >> /var/log/zer0fill/drivelog.txt
+	# run command to zero fill drive selected
 	sudo dd if=/dev/zero if=/dev/$drivenum bs=1M > /dev/null
+# if user doesn't respond with y or n script ends
 else
-	echo "error"
+	echo "user input did not match y or n"
 	exit
 fi
 exit
